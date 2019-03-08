ca952f54b5378b3cf0c43bccba68a902164b85a0
root <root@ip-10-0-0-107.ap-northeast-2.compute.internal>
2019-03-08 09:57:21 +0000

    Finish bugfix and able to rnu

--- bellmanford.py
+++ bellmanford.py
@@ -2,17 +2,18 @@ import redis
 import time
 import traceback
 import datetime
+import json
 
 # Define redisStarter
 r = redis.StrictRedis(host='localhost', port=6379)                          # Connect to local Redis instance
 
 def Generatebellmanford():
     print("Begin table construction")
-    keys = redis.keys('*')
+    keys = r.keys('*')
     table = []
     i = 0
     for key in keys:
-        if redis.type(key) == KV:
-            table[key] = redis.get(key)
+        print(json.loads(r.get(key)))
+        # valuedump = r.get(key)
 
 Generatebellmanford()
