d5069abb1be0cacbb3d2180ee208bfacac1c7678
Jacky Chan <chankongching@gmail.com>
2019-03-12 13:41:15 +0800

    Adding bellman code

--- bellmanford.py
+++ bellmanford.py
@@ -3,6 +3,7 @@ import time
 import traceback
 import datetime
 import json
+import bellman
 
 # Define redisStarter
 r = redis.StrictRedis(host='localhost', port=6379)                          # Connect to local Redis instance
@@ -13,10 +14,12 @@ def generateGraph():
     print("Begin table construction")
     keys = r.keys('*')
     for key in keys:
+        # Get pair name, bid/ask price and volume then store into bellman_ford readable graph
         pairname = key.decode("utf-8").split('DEPTH',1)[0]
         valuedump = json.loads(r.get(key))
         key1 = pairname[:3]
         key2 = pairname[-3:]
+        # Init Json item if it doesnt exist
         if not graph_askprice.get(key1):
             graph_askprice[key1] = {}
         if not graph_askprice.get(key2):
@@ -29,22 +32,18 @@ def generateGraph():
         askprice2 = valuedump['bids'][0][0]
         volume1 = valuedump['asks'][0][1]
         volume2 = valuedump['bids'][0][1]
-
-        # print(valuedump)
-        # print(key1 + ':' + key2)
+        # Store value into corresponding position
+        # Say, HKDJPY need to be saved into {'HKD':{'JPY':PRICE},'JPY':{'HKD':PRICE}}
         graph_askprice[key1].update({key2:float(askprice1)})
         graph_askprice[key2].update({key1:(1/float(askprice2))})
         graph_volume[key1].update({key2:volume1})
-        graph_volume[key2].update({key1.volume2})
-
-        # graph_askprice[key1].update({key2:valuedump['ask'][0][0][1]})
-        # # table[key] = (json.loads(r.get(key)))
-        # # valuedump = r.get(key)
+        graph_volume[key2].update({key1:volume2})
+     return graph_askprice, graph_volume
 
 def Generatebellmanford():
     print("Placeholder")
 
 # Generatebellmanford()
-generateGraph()
+graph_askprice, graph_volume = generateGraph()
 print(graph_askprice)
 print(graph_volume)
