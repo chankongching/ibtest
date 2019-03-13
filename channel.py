615b713ae5098469dd9c585218f5dfbaf5c2159e
Jacky Chan <chankongching@gmail.com>
2019-03-13 10:01:35 +0800

    Get predecessor running

--- channel.py
+++ channel.py
@@ -6,15 +6,6 @@ import datetime
 # Define redisStarter
 r = redis.StrictRedis(host='localhost', port=6379)                          # Connect to local Redis instance
 
-def Generatebellmanford():
-    print("Begin table construction")
-    keys = redis.keys('*')
-    table = []
-    i = 0
-    for key in keys:
-        if redis.type(key) == KV:
-            table[key] = redis.get(key)    
-
 def RedisCheck():
     try:
         p = r.pubsub()                                                              # See https://github.com/andymccurdy/redis-py/#publish--subscribe
--- exec.py
+++ exec.py
@@ -43,25 +43,57 @@ def generateGraph():
         graph_volume[key2].update({key1:volume2})
     return graph_askprice, graph_volume
 
-def generatebellmanford():
-    print("Placeholder")
+def searchbybellmanford(graph_askprice,graph_volume):
+    ### Upgrade code by updating graph_askprice for affected node only
+    # for base in graph_askprice:
+    #     d,p = bellman.bellman_ford(graph_askprice, base)
+    #     # Remove base
+    #     for currency in p:
+    #         if base in currency:
+    #             del p[base]
+    #     # Remove direct predecessor
+    #     filtered = filter(base, p)
+    #     for currency in filtered:
+    #         del[currency]
+    base = 'HKD'
+    d,p = bellman.bellman_ford(graph_askprice, base)
+    print('Printing predecessor from searchbybellmanford: ')
+    print(p)
+    # Remove base
+    for currency in p:
+        if base in currency:
+            del p[base]
+    print('After first removal')
+    print(p)
+    # Remove direct predecessor
+    filtered = filter(base, p)
+    for currency in filtered:
+        del p[currency]
+    print('After second removal')
+    print(p)
+    return p
 
 # Generatebellmanford()
-graph_askprice, graph_volume = generateGraph()
-d1, p1 = bellman.bellman_ford(graph_askprice, 'HKD')
-d2, p2 = bellmanford.bellman_ford(graph_askprice, 'HKD')
-print('Graph Ask Price:')
-print(json.dumps(graph_askprice,indent=4, sort_keys=True))
-print('Graph Volume')
-print(graph_volume)
-print('First Approach')
-print('Distance = ')
-print(json.dumps(d1,indent=4, sort_keys=True))
-print('Predecessor = ')
-print(json.dumps(p1,indent=4, sort_keys=True))
-print('##############################################################')
-print('Second Approach')
-print('Distance = ')
-print(json.dumps(d2,indent=4, sort_keys=True))
-print('Predecessor = ')
-print(json.dumps(p2,indent=4, sort_keys=True))
+
+if __name__ == '__main__':
+    graph_askprice, graph_volume = generateGraph()
+    d1, p1 = bellman.bellman_ford(graph_askprice, 'HKD')
+    d2, p2 = bellmanford.bellman_ford(graph_askprice, 'HKD')
+    print('Graph Ask Price:')
+    print(json.dumps(graph_askprice,indent=4, sort_keys=True))
+    print('Graph Volume')
+    print(graph_volume)
+    print('##############################################################')
+    print('First Approach')
+    print('Distance = ')
+    print(json.dumps(d1,indent=4, sort_keys=True))
+    print('Predecessor = ')
+    print(json.dumps(p1,indent=4, sort_keys=True))
+    print('##############################################################')
+    print('Second Approach')
+    print('Distance = ')
+    print(json.dumps(d2,indent=4, sort_keys=True))
+    print('Predecessor = ')
+    print(json.dumps(p2,indent=4, sort_keys=True))
+    print('##############################################################')
+    searchbybellmanford(graph_askprice,graph_volume)
