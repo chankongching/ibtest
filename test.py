c942c10691c00b8997ac47b8d29029049c17225b
Jacky Chan <chankongching@gmail.com>
2019-03-14 18:58:01 +0800

    Fixing indent issue

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import exec
-    graph_bidprice, graph_bidprice_inverse, graph_volume = exec.generateGraph()
-    # Use inverse to calculate nodes graph_bidprice_inverse
-    nested_d, nested_p = exec.generatebellmanford(graph_bidprice)
-    print('graph_volume = ')
-    print(graph_volume)
+
+graph_bidprice, graph_bidprice_inverse, graph_volume = exec.generateGraph()
+# Use inverse to calculate nodes graph_bidprice_inverse
+nested_d, nested_p = exec.generatebellmanford(graph_bidprice)
+print('graph_volume = ')
+print(graph_volume)
