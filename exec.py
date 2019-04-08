6e9b3a6f35f4609e713d99e47a7013b2c08c545d
Jacky Chan <chankongching@gmail.com>
2019-04-08 11:39:59 +0800

    Add price check for bellman too

--- exec.py
+++ exec.py
@@ -543,7 +543,7 @@ def wrapper():
         # f.write('Test message' + str(datetime.datetime.now()))
         f.write(str(datetime.datetime.now()) + '!' + json.dumps(orders) + '\n')
         f.close()
-        f = open('orders_pricecheck.txt','a')
+        f = open('orders_loop_pricecheck.txt','a')
         # Print result for checking
         f.write(str("graph_bidprice = " + '\n'))
         f.write(str(json.dumps(graph_bidprice,indent=4, sort_keys=True) + '\n'))
@@ -569,6 +569,11 @@ def wrapper():
         f = open('orders_bellman.txt','a')
         # f.write('Test message' + str(datetime.datetime.now()))
         f.write(str(datetime.datetime.now()) + '!' + json.dumps(orders) + '\n')
+        f.close()
+        f = open('orders_bellman_pricecheck.txt','a')
+        # Print result for checking
+        f.write(str("graph_bidprice = " + '\n'))
+        f.write(str(json.dumps(graph_bidprice,indent=4, sort_keys=True) + '\n'))
         f.close()
 
 
